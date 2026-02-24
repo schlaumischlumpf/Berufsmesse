31f645de5480111812f8c95ae36547ce21b94707
TheUnknown-0 <minecraft-mo@posteo.de>
2026-02-24 10:36:43 +0100

    Update default DB_PASS in config.php
    Change default database password to 'test'.

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Datenbankkonfiguration
 define('DB_HOST', getenv('DB_HOST') ?: 'berufsmesse_db');
 define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_PASS', getenv('DB_PASS') ?: 'test');
 define('DB_NAME', getenv('DB_NAME') ?: 'berufsmesse');
 
 // Zeitzone
